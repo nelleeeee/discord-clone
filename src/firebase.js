e35cb0446c7a7c77ba7d1a1db446af34b04479ff
nelleeeee <rudghksldl@gmail.com>
2020-10-13 02:15:13 +0900

    sidebar ~ing

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDSktol8Vl9YV3fR2hLzUZPzCye9Jqor14",
+  authDomain: "discord-clone-31712.firebaseapp.com",
+  databaseURL: "https://discord-clone-31712.firebaseio.com",
+  projectId: "discord-clone-31712",
+  storageBucket: "discord-clone-31712.appspot.com",
+  messagingSenderId: "926713157759",
+  appId: "1:926713157759:web:0216780f3dda7ae6ca474a",
+  measurementId: "G-EE38VYYBKE",
+};
